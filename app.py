a7e22ad29585f62b36543ec1c022240e613f5004
kim-ong <102169775+kim-ong@users.noreply.github.com>
2022-09-04 04:59:24 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ app = Flask(__name__)
 import tensorflow as tf 
 import matplotlib.pyplot as plt 
 import numpy as np
-import math
+#import math
+import re
 
-from keras.models import load_model 
-from keras.backend import set_session
+from tensorflow.keras.models import load_model 
+from tensorflow.keras.utils import load_img
 from skimage.transform import resize 
 
 print("Loading model") 
@@ -28,18 +29,24 @@ def main_page():
 
 @app.route('/prediction/<filename>') 
 def prediction(filename):
-    actual = re.search(r"\d",filename)
-    #print(actual.group())
-    my_img = plt.imread(os.path.join('uploads', filename))
-    img = resize(my_img, (128, 128, 1))
+    number = re.search(r"\d",filename)
+    actual = number.group()
+    #my_img = plt.imread(os.path.join('uploads', filename))
+    my_file = os.path.join('uploads', filename)
+    img = load_img(my_file, color_mode="grayscale", target_size=(28,28))
+    img = np.invert(img)
+    img = img.astype('float32')
+    img_re = img.reshape(28,28,1)
+    img_re /= 255
+    img_re = resize(img_re, (28, 28, 1))
     model.run_eagerly=True
-    probabilities = model.predict(np.array( [img,] ))[0,:]
+    probabilities = model.predict(np.array( [img_re,] ))[0,:]
     print(probabilities)
     number_to_class = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     index = np.argsort(probabilities)
-    marks = math.trunc(probabilities[index[9]] * 100)
+    #marks = math.trunc(probabilities[index[9]] * 100)
     if number_to_class[index[9]] == actual:
-     grade = "Good Job! Your grade is marks"
+     grade = "Good Job!"
     else:
      grade = "Hmmm ... Did I make a wrong guess?"
     predictions = {
